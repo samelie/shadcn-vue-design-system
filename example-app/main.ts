0e082b44524dbc670b8248d5e0cf248389dc1eb3
Sam Elie <samradelie@gmail.com>
2025-10-17 09:51:05 -0700

    0.0.1 vue

--- example-app/main.ts
+++ example-app/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import '../src/style/styles.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "../src/style/styles.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
