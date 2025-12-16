9d883eef2849939031e2ee20fa1cf7eb30a7b631
Sam Elie <samradelie@gmail.com>
2025-12-15 19:59:20 -0800

    big

---
+++ src/components/custom/IconButtonTooltip.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { TooltipContentProps } from "reka-ui"
+import type { HTMLAttributes, Component } from "vue"
+import type { ButtonVariants } from "../ui/button"
+import { Button } from "../ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "../ui/tooltip"
+
+interface Props {
+  icon: Component
+  label: string
+  variant?: ButtonVariants["variant"]
+  size?: ButtonVariants["size"]
+  side?: TooltipContentProps["side"]
+  sideOffset?: number
+  buttonClass?: HTMLAttributes["class"]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: "ghost",
+  size: "icon",
+  side: "top",
+  sideOffset: 4,
+})
+
+const emit = defineEmits<{
+  click: [event: MouseEvent]
+}>()
+</script>
+
+<template>
+  <Tooltip>
+    <TooltipTrigger as-child>
+      <Button
+        :variant="variant"
+        :size="size"
+        :aria-label="label"
+        :class="buttonClass"
+        @click="emit('click', $event)"
+      >
+        <component :is="icon" />
+      </Button>
+    </TooltipTrigger>
+    <TooltipContent :side="side" :side-offset="sideOffset">
+      <p>{{ label }}</p>
+    </TooltipContent>
+  </Tooltip>
+</template>
---
+++ src/components/custom/index.ts
@@ -0,0 +1,1 @@
+export { default as IconButtonTooltip } from "./IconButtonTooltip.vue";
